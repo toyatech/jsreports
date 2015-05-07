9f24cb5503bcd1c892896c822a00b01ec796fbde
Dusty Little <dlittle@toyatech.net>
2015-05-07 15:03:13 -0500

    continued work on per node type processors

--- jrxml2json.js
+++ jrxml2json.js
@@ -42,7 +42,11 @@
   };
 
   var collapseNodes = [
-    'jasperReport'
+    'jasperReport', '#cdata-section'
+  ];
+ 
+  var mergeNodes = [
+    'reportElement', 'graphicElement', 'textElement'
   ];
 
   var typeMap = {
@@ -152,30 +156,6 @@
     return value;
   }
 
-  function formatBandElement(item) {
-    var obj = {}, band = item.getElementsByTagName('band')[0];
-    obj['height'] = band.getAttribute('height');
-    if (band.hasChildNodes()) {
-      obj.elements = [];
-      for (var i = 0; i < band.childNodes.length; i++) {
-        var item = band.childNodes.item(i);
-        if (item.nodeType == 1) {
-          var element = {};
-          element['type'] = item.nodeName;
-          for (var j = 0; j < item.childNodes.length; j++) {
-            var elem = item.childNodes.item(j);
-            //console.log('bandElem: ' + elem);
-            //if (item.nodeType == 1) {
-            //  formatAttributes(elem, element);
-            //} 
-          }
-          obj.elements.push(element);
-        }
-      }
-    }
-    return obj;
-  }
-
   function hasTextOrCDATASection(node) {
     if (node.hasChildNodes()) {
       var nodeType = node.firstChild.nodeType;
@@ -183,25 +163,61 @@
     }
   }
 
+  function hasOwnProperties(obj) {
+    for (var k in obj) {
+      if (obj.hasOwnProperty(k)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function mergeProperties(obj1, obj2) {
+    var obj3 = {};
+    for (var k in obj1) { obj3[k] = obj1[k]; }
+    for (var k in obj2) { obj3[k] = obj2[k]; }
+    return obj3;
+  }
+
   function processChildNodes(node, obj) {
     if (node.hasChildNodes()) {
       for (var i = 0; i < node.childNodes.length; i++) {
         var item = node.childNodes.item(i);
         var nodeName = format(item.nodeName, 'elementName');
-        console.log(nodeName + ', ' + item.nodeType);
-        if (obj && typeof(obj[nodeName]) == "undefined") {
-          if (!(collapseNodes.indexOf(nodeName) > -1)) {
-            obj = process(item, {});
-          } else {
+        if (collapseNodes.indexOf(nodeName) > -1) {
+          obj = process(item, {});
+        } else if (mergeNodes.indexOf(nodeName) > -1) {
+            obj = mergeProperties(obj, process(item, {}));
+        } else if (bandElements.indexOf(nodeName) > -1) {
+          var tmp = {}, band = item.getElementsByTagName('band')[0];
+          tmp['height'] = band.getAttribute('height');
+          if (band.hasChildNodes()) {
+            tmp.elements = [];
+            for (var j = 0; j < band.childNodes.length; j++) {
+              var element = band.childNodes.item(j);
+              if (element.nodeType == 1) {
+                if (element.hasChildNodes()) {
+                for (var k = 0; j < element.childNodes.length; k++)
+                  var elementDetail = element.childNodes.item(k);
+                  if (elementDetail.nodeType == 1) {
+                    //tmp.elements.push(mergeProperties({type: element.nodeName}, process(elementDetail)));
+                  }
+                }
+              }
+            }
+          }
+          obj[nodeName] = tmp; 
+        } else {
+          if (typeof(obj[nodeName]) == "undefined") {
             obj[nodeName] = process(item, {});
+          } else {
+            if (typeof(obj[nodeName].push) == "undefined") {
+              var old = obj[nodeName];
+              obj[nodeName] = [];
+              obj[nodeName].push(old);
+            }
+            obj[nodeName].push(process(item, {}));
           }
-        } else if (obj) {
-          if (typeof(obj[nodeName].push) == "undefined") {
-            var old = obj[nodeName];
-            obj[nodeName] = [];
-            obj[nodeName].push(old);
-          }
-          obj[nodeName].push(process(item, {}));
         } 
       }
     }
@@ -242,9 +258,7 @@
     return obj;
   }
   
-  function skip() {
-    return;
-  }
+  function skip() { return; }
 
   var processors = {
     element: [
@@ -260,20 +274,16 @@
     cdata_section: [
       processCDATASection
     ], 
-    entity_reference: [], 
-    entity: [], 
-    processing_instruction: [
-      skip
-    ],
-    comment: [],
+    entity_reference: [skip], 
+    entity: [skip], 
+    processing_instruction: [skip],
+    comment: [skip],
     document: [
       processChildNodes
     ],
-    document_type: [
-      skip
-    ],
-    document_fragment: [],
-    notation: []
+    document_type: [skip],
+    document_fragment: [skip],
+    notation: [skip]
   };
 
   function process(node, obj) {
